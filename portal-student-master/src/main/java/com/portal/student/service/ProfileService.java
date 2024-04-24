308a4d828b27da86f7488fece35980df9f492a96
Oluwagbemiga99 <119671728+Oluwagbemiga99@users.noreply.github.com>
2024-04-24 10:39:39 +0100

    Update ProfileService.java

--- portal-student-master/src/main/java/com/portal/student/service/ProfileService.java
+++ portal-student-master/src/main/java/com/portal/student/service/ProfileService.java
@@ -32,6 +32,6 @@ public class ProfileService {
             return "Profile successfully updated";
         }
 
-        return "Student is not exist";
+        return "Student does not exist";
     }
 }
